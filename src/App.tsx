a119fa574fec4e77b53672979a6e3d6d1cd06699
Sven-27 <github.geometric664@passinbox.com>
2026-05-25 14:53:57 +0200

    finished thank you component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Rating from './compoents/Rating'
+import Thanks from './compoents/Thanks'
 
 function App() {
 
   return (
     <main className="w-[clamp(327px,60vw,412px)] bg-gradient-to-b from-[#232a34] to-[#181e27] rounded-2xl sm:rounded-3xl p-[24px] sm:py-[32px]">
-      <Rating />
+      {/* <Rating /> */}
+      <Thanks />
     </main>
   )
 }
